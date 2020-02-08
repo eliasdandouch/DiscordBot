e1cc00790ceec39ef9952c2ec73270bdae8c12a7
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-08 13:28:00 -0800

    has auto-delete feature

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -6,17 +6,20 @@ import os
 class AdminCommands(commands.Cog):
     def __init__(self,client):
         self.client = client
+    channels = ['general']
 
     @commands.command()
     @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=None)
+        await ctx.message.delete()
         await ctx.author.send(f'{member.mention}, has been successfully kicked from the server.')
 
     @commands.command()
     @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
             await member.ban(reason=None)
+            await ctx.message.delete()
             await ctx.author.send(f'{member.mention}, has been successfully banned from the server.')
 
 def setup(client):
