6a420d5bac34fa55f6d8be8ffad3a1ccbf57827b
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-08 13:29:16 -0800

    flip command now works in DM

--- help/Help.py
+++ help/Help.py
@@ -2,23 +2,28 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import os
+
 class help(commands.Cog):
     def __init__(self,client):
         self.client = client
-
+#Sends User Embeded Message w/ list of bot commands and what they do.
     @commands.Cog.listener()
     async def on_message(self,message):
+        channels = ['general']
         embed = discord.Embed(
-        title = 'LouieBot'
+        title = 'Bot Commands',
+        colour = discord.Colour.blue()
         )
-        embed.add_field(name='About', value="This Bot is very simple and is not intended to be complex or to have complex features. This bot was designed to pervent spam in the chat, So simply every command you type will be cleared and the result of the command will be DM'd to you. \nhttps://cdn.discordapp.com/attachments/672922054500024323/675232581322801172/tenor.gif", inline=True)
-        if message.content.startswith('.about'):
-            await message.delete()
+        embed.set_author(name='LouieBot')
+        embed.add_field(name='$flip', value='Flips a coin',inline=True)
+        embed.add_field(name='$ping', value='Tells you the ping of the bot (Admin Only)', inline=True)
+        embed.add_field(name='$clear', value='$clears previous command, this command can be given a value. (ex:$clear amount) (Admin Only)', inline=True)
+        embed.add_field(name='.about', value='Tells you the purpose of the Bot!', inline=True)
+        if message.content.startswith('.help'):
             await message.author.send(embed=embed)
-
-
-
-
+            if str(message.channel) in channels:
+                if message.content.startswith('.help'):
+                    await message.delete()
 
 def setup(client):
     client.add_cog(help(client))
