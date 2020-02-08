cdc317af39a39e9264ed43ae4f29da6d7f544645
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-07 16:06:58 -0800

    Added a help cog

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-import youtube_dl
 import os
-import random
-import re
 
-client = commands.Bot(command_prefix = ',')
 
+client = commands.Bot(command_prefix = '$')
+#Loads Basic Command Cogs
 client.load_extension('cogs.MiscCommands')
 client.load_extension('cogs.AdminCommands')
-client.load_extension('cogs.BotOnline')
+#Loads Bad Word Filter Cog5
 client.load_extension('language.BadLanguage1')
 client.load_extension('language.BadLanguage2')
+client.load_extension('language.BadLanguage3')
+client.load_extension('language.BadLanguage4')
+client.load_extension('language.BadLanguage5')
+client.load_extension('language.BadLanguage6')
+client.load_extension('language.BadLanguage7')
+client.load_extension('language.BadLanguage8')
+client.load_extension('language.BadLanguage9')
+#Loads Help Commands Cogs
+client.load_extension('help.About')
+client.load_extension('help.Help')
+
+##Bot Status
+@client.event
+async def on_ready():
+    game = discord.Game('Type ".help" in general for Bot commands')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print("The bot is online and has connected to the server successfully.")
+
+
 
 #Calling Discord Account
-client.run("TOKEN")
+client.run("NjcyODc2MjQ3ODA5OTE2OTQ5.XjYzig.mr3pMaHZgx4MmE1RLMCbAq75WgY")
