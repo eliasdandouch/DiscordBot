9589643b00dca57b3c7d3ba00ed487234cb75101
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-07 16:06:33 -0800

    Now has permissions for $kick & $ban

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -8,12 +8,16 @@ class AdminCommands(commands.Cog):
         self.client = client
 
     @commands.command()
+    @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=None)
+        await ctx.author.send(f'{member.mention}, has been successfully kicked from the server.')
 
     @commands.command()
+    @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
-        await member.ban(reason=None)
+            await member.ban(reason=None)
+            await ctx.author.send(f'{member.mention}, has been successfully banned from the server.')
 
 def setup(client):
     client.add_cog(AdminCommands(client))
