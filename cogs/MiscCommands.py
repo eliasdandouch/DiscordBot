820873789c74d6966a7030aa973bd86dddf6033b
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-07 16:19:23 -0800

    $ping & $clear now have perms

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -11,10 +11,12 @@ class MiscCommands(commands.Cog):
 
     #MiscCommands
     @commands.command()
+    @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def ping(self,ctx):
         await ctx.send("Pong!")
 
     @commands.command()
+    @commands.has_any_role('Elias','Gavin', 123206185326215169)
     async def clear(self,ctx,amount=3):
         await ctx.channel.purge(limit=amount)
 
