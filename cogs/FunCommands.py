1c73f303375b359eee6c511f4388477a6fe6191d
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-14 00:33:45 -0800

    v.04

--- cogs/FunCommands.py
+++ cogs/FunCommands.py
@@ -22,7 +22,7 @@ class FunCommands(commands.Cog):
     @commands.command()
     async def magicball(self,ctx,message : str = None):
         if message == None:
-            await ctx.author.send("For the 8Ball command to work you must enter a message after the command."))
+            await ctx.author.send("For the 8Ball command to work you must enter a message after the command.")
             await ctx.message.delete()
         else:
             await ctx.author.send(random.choice(['It is certain.','It is decidedly so.',' Without a doubt.','Yes - definitely.','You may rely on it.','As I see it, yes.','Most likely.',' Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.',' Cannot predict now.','Concentrate and ask again.',"Don't count on it.","My reply is no."," My sources say no.","Outlook not so good.",'Very doubtful.']))
