f4b9b53789ba57840b55038bf651f96f91ac8085
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-13 16:10:43 -0800

    Now has 8ball command

--- cogs/FunCommands.py
+++ cogs/FunCommands.py
@@ -28,6 +28,7 @@ class FunCommands(commands.Cog):
             await ctx.message.delete()
         else:
             await ctx.author.send(random.choice(['It is certain.','It is decidedly so.',' Without a doubt.','Yes - definitely.','You may rely on it.','As I see it, yes.','Most likely.',' Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.',' Cannot predict now.','Concentrate and ask again.',"Don't count on it.","My reply is no."," My sources say no.","Outlook not so good.",'Very doubtful.']))
+            await ctx.message.delete()
 
 def setup(client):
     client.add_cog(FunCommands(client))
