9bf9b313e598e49847921c45bd918ed811258775
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-02-13 18:53:23 -0800

    Now tells the console the error

--- cogs/Error.py
+++ cogs/Error.py
@@ -11,7 +11,8 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self,ctx,error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.author.send(f"Oops seems like you've eneted an invalid command using the {0.command.qualified_name} please type '$help' for the list of commands.")
+            await ctx.author.send("Oops seems like you've eneted an invalid command using the please type '$help' for the list of commands.")
+            await print(print('Error in {0.command.qualified_name}: {1}'.format(ctx, error)))
             await ctx.message.delete()
 
 def setup(client):
