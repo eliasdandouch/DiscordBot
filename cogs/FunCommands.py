bf7b025215d870f9f7daa32571ffa344f1619b1f
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-15 23:23:41 -0800

    t

--- cogs/FunCommands.py
+++ cogs/FunCommands.py
@@ -32,7 +32,7 @@ class FunCommands(commands.Cog):
             await ctx.message.delete()
 
     @commands.command()
-    async def convert(self,ctx,conversion1: str = None,conversion2: str = None,*, number: int = None, a,b,c): #a = 9, b = 5, c = 32
+    async def convert(self,ctx,conversion1: str = None,conversion2: str = None, number: int = None): #a = 9, b = 5, c = 32
         if conversion1 and conversion2 and number == None: #if the user doesnt type two converters and a number it will delete the message.
             await ctx.message.delete()
         if conversion1 == None: #if the user does not type one of the converters it will delete the message.
