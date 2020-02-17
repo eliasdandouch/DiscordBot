4cc57e6d06f1185e01586b09c753014a957f356d
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-16 19:43:06 -0800

    test

--- cogs/commands/reddit.py
+++ cogs/commands/reddit.py
@@ -8,8 +8,8 @@ class reddit(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-    @commands.command()
-    async def reddit(self,ctx,message: str = None):
+    @commands.Cog.listener()
+    async def reddit(self,ctx,message):
         reddit = praw.Reddit(client_id='IifJXvTx06bvzQ', \
                      client_secret='H-649AGpK9NuCcJOkXlGWBdIkSQ', \
                      user_agent='LouieBot', \
@@ -18,8 +18,9 @@ class reddit(commands.Cog):
         subreddit = reddit.subreddit('memes')
         top_subreddit = subreddit.top(limit=100)
         for submission in reddit.subreddit('memes').hot(limit=100):
-            await ctx.message.delete()
-            await ctx.author.send(top_subreddit)
+            if message.content.lower('$reddit'):
+                await ctx.message.delete()
+                await ctx.author.send(top_subreddit)
 
 def setup(client):
     client.add_cog(reddit(client))
