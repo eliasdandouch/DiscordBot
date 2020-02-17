5421ed23ae8f3db04893ada26c99089d15189a5b
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-16 22:34:53 -0800

    test

--- cogs/commands/reddit.py
+++ cogs/commands/reddit.py
@@ -8,18 +8,18 @@ class reddit(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_message(self,message):
+    @commands.command()
+    async def reddit(self,ctx, message: str = None):
         reddit = praw.Reddit(client_id='IifJXvTx06bvzQ', \
                      client_secret='H-649AGpK9NuCcJOkXlGWBdIkSQ', \
                      user_agent='LouieBot', \
                      username='LouieKB', \
                      password='yeetus450036235')
         subreddit = reddit.subreddit('memes')
-        top_subreddit = subreddit.top(limit=100)
-        if message.content.startswith('reddit'):
-            await message.delete()
-            await message.author.send(top_subreddit)
+        top_subreddit = subreddit.top(limit=25)
+        if message == None:
+            await ctx.author.send(random.choice(top_subreddit))
+            await ctx.message.delete()
 
 def setup(client):
     client.add_cog(reddit(client))
