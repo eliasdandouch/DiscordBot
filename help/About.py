ec39b74a149e79fea23f56f5c9420ed89f7a34dd
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-17 20:31:28 -0800

    added dev

--- help/About.py
+++ help/About.py
@@ -21,7 +21,7 @@ class About(commands.Cog):
 
     async def dev(self,ctx,arg: int = None):
         if arg == None:
-            await ctx.author.send(f'{mention.user}, This Bot was developed by Louie#0002!')
+            await ctx.author.send('This Bot was developed by Louie#0002!')
             await ctx.message.delete()
         else:
             await ctx.author.send("Oops seems like you've entered an invalid command, please use $help for the list of the bot's commands.")
