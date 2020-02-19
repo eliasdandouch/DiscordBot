4fe194da990cddc2098c55a5bbeb8a7ad8850a5a
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-18 20:17:43 -0800

    updated

--- cogs/commands/FunCommands.py
+++ cogs/commands/FunCommands.py
@@ -32,7 +32,7 @@ class FunCommands(commands.Cog):
             await ctx.author.send(random.choice(['It is certain.','It is decidedly so.',' Without a doubt.','Yes - definitely.','You may rely on it.','As I see it, yes.','Most likely.',' Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.',' Cannot predict now.','Concentrate and ask again.',"Don't count on it.","My reply is no."," My sources say no.","Outlook not so good.",'Very doubtful.']))
             await ctx.message.delete()
 
-    @commands.command()
+    @commands.command(aliases=str.upper['c'])
     async def convert(self,ctx,conversion1: str.upper = None,conversion2: str.upper = None, number: int = None):
         mp = ("Missing parameterer, please type $help for the list of commands.")
         if conversion1 and conversion2 and number == None: #if the user doesnt type two converters and a number it will delete the message.
