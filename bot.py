8f5f4787faa6a37fee9c6d64fc501b136783faad
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-02-28 18:02:44 -0800

    .

--- bot.py
+++ bot.py
@@ -41,8 +41,6 @@ async def on_ready():
     game = discord.Game('Yeet | $commands')
     print('-------------------------------------------------------------')
     await client.change_presence(status=discord.Status.online, activity=game)
-    logging.basicConfig(filename='louiebot.log', level=logging.INFO)
-    logging.info('The Start of The Bot Logs')
 
 
 #Calling Discord Account
