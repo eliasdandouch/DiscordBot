75b91931ef72a0380eb38d830feb91e1d42d6d66
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-03-02 21:59:16 -0800

    added mcip

--- commands/MinecraftServer.py
+++ commands/MinecraftServer.py
@@ -12,7 +12,7 @@ class MinecraftServer(commands.Cog):
     async def mc(self,ctx,password: str = os.environ['PASSWORD'],*,mcserver: str = os.environ['MCSERVER']):
         if password == os.environ['PASSWORD']:
             await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password")
-            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ.['MCSERVER']}")
+            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['MCSERVER']}")
             await ctx.message.delete()
         elif password.upper is not os.environ['PASSWORD']:
             await ctx.author.send(f"{ctx.message.author.mention}, Incorrect! Either you've typed the password incorrectly or you were given a bad password. Please contact @Louie#0002.")
