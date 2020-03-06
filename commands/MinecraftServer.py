80b389858669d09acb99627ac3bdeb6c1a433ece
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-03-05 23:14:15 -0800

    updated.

--- commands/MinecraftServer.py
+++ commands/MinecraftServer.py
@@ -9,11 +9,17 @@ class MinecraftServer(commands.Cog):
         self.client = client
 
     @commands.command(aliases=['minecraft','mcip'])
-    async def mc(self,ctx,password: str = os.environ['PASSWORD'],*,mcserver: str = os.environ['MCSERVER']):
-        if password == os.environ['PASSWORD']:
+    async def mc(self,ctx,password: str = os.environ['PASSWORD'],server: str = None,*,vanillaserver: str = os.environ['VANILLASERVER'],ftbserver: str = os.environ['FTBSERVER']):
+        if password == os.environ['PASSWORD'] and server == 'vanilla':
             await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password")
-            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['MCSERVER']}")
+            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['VANILLASERVER']}")
             await ctx.message.delete()
+        elif password == os.environ['PASSWORD'] and server == 'ftb':
+            await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password")
+            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['VANILLASERVER']}")
+            await ctx.message.delete()
+        elif password == os.environ['PASSWORD'] and server == None:
+            await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password BUT I can't give you an IP if I don't know which server please specificy! $mcip (ftb,vanilla)")
         elif password.upper is not os.environ['PASSWORD']:
             await ctx.author.send(f"{ctx.message.author.mention}, Incorrect! Either you've typed the password incorrectly or you were given a bad password. Please contact @Louie#0002.")
             await ctx.message.delete()
