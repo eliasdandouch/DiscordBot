beccdd7265d1666da18ec98769bb839c393356bf
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2020-03-05 23:19:32 -0800

    updated.

--- commands/MinecraftServer.py
+++ commands/MinecraftServer.py
@@ -12,11 +12,11 @@ class MinecraftServer(commands.Cog):
     async def mc(self,ctx,password: str = os.environ['PASSWORD'],server: str = None,*,vanillaserver: str = os.environ['VANILLASERVER'],ftbserver: str = os.environ['FTBSERVER']):
         if password == os.environ['PASSWORD'] and server == 'vanilla':
             await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password")
-            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['VANILLASERVER']}")
+            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the Vanilla Sever is {os.environ['VANILLASERVER']}")
             await ctx.message.delete()
         elif password == os.environ['PASSWORD'] and server == 'ftb':
             await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password")
-            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the sever is {os.environ['VANILLASERVER']}")
+            await ctx.author.send(f"{ctx.message.author.mention}, the IP to the Modded FTB Direwolf 1.12 Sever is {os.environ['VANILLASERVER']}")
             await ctx.message.delete()
         elif password == os.environ['PASSWORD'] and server == None:
             await ctx.author.send(f"{ctx.message.author.mention}, Congratulations! You've entered the right password BUT I can't give you an IP if I don't know which server please specificy! $mcip (ftb,vanilla)")
