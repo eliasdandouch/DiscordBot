8764af358fa676e863e8529f7a2eb87be6bdaee7
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-04-01 16:57:50 -0700

    fixed

--- errors/Error.py
+++ errors/Error.py
@@ -14,7 +14,7 @@ class Error(commands.Cog):
             await ctx.author.send("Oops seems like your messing an argument for this command, please type $help for the list of commands!")
             await ctx.message.delete()
     #A simple error handle if a user tries to pass a command that does not exist it will give them an error message.
-    @Commands.Cog.listener()
+    @commands.Cog.listener()
     async def on_command_error(self,ctx,error):
         if isinstance(error, commands.CommandNotFound):
             await ctx.author.send(f"{ctx.author.metion}, the command you tried to enter does not exist! Please type $help for the list of commands!")
