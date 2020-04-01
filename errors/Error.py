00a9f3f7163f87c188dbda3d77c91c6a3e63c778
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2020-04-01 16:55:29 -0700

    Added CommandNotFound

--- errors/Error.py
+++ errors/Error.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks,commands
 from discord.utils import get
 import os
 
@@ -11,7 +11,13 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self,ctx,error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.author.send("Oops seems like you've eneted an invalid command using the please type '$help' for the list of commands.")
+            await ctx.author.send("Oops seems like your messing an argument for this command, please type $help for the list of commands!")
+            await ctx.message.delete()
+    #A simple error handle if a user tries to pass a command that does not exist it will give them an error message.
+    @Commands.Cog.listener()
+    async def on_command_error(self,ctx,error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.author.send(f"{ctx.author.metion}, the command you tried to enter does not exist! Please type $help for the list of commands!")
             await ctx.message.delete()
 
 def setup(client):
